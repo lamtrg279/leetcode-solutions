d2375e73f4e853548d66865e2731aad4eb1994d1
Lam Truong <nl2791999@gmail.com>
2022-07-18 20:15:16 -0500

    first commit

---
+++ 94.binary-tree-inorder-traversal.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=94 lang=javascript
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let current = root;
+  let stack = [];
+  let result = [];
+
+  while (current || stack.length !== 0) {
+    while (current) {
+      stack.push(current);
+      current = current.left;
+    }
+    current = stack.pop();
+    result.push(current.val);
+    current = current.right;
+  }
+  return result;
+};
+// @lc code=end
