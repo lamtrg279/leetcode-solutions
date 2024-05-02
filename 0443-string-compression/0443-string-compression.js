054b50aa518cc948b5accde33ad2500495ae5757
lamtruong <76791917+lamtrg279@users.noreply.github.com>
2024-05-02 12:00:29 -0500

    Update 0443-string-compression.js

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -8,11 +8,14 @@ var compress = function (chars) {
     while (j < chars.length) {
         let count = 0
         let curr = chars[j]
-        while (chars[j] === curr) {
+        while (chars[j] === curr) {    //count how many repeated char
             j++
             count++
         }
-        chars[i] = curr
+        chars[i] = curr            /*this is the difficult point:
+Let's say [a,a,a,a,a,b,b] then chars[i] = curr doesn't make sense at the first passbecause chars[i] is 'a' and curr is 'a' at both index 0.
+But from the second pass, curr is now 'b' because the final array must be [a,5,b,2], if we don't include this line of code then chars[i] is still at index 2 while curr is at index 5.
+*/
         i++
         if (count > 1) {
             for (let digit of count.toString()) {
@@ -21,6 +24,5 @@ var compress = function (chars) {
             }
         }
     }
-    console.log(i)
     return i
 };
